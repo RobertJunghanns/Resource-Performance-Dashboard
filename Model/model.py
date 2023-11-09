2ff3d1092360d163618a5e87b6f248b2b7f0bbbc
RobertJunghanns <51117160+RobertJunghanns@users.noreply.github.com>
2023-11-09 17:11:33 +0100

    created first Hello World Dashboard with MVC

--- Model/model.py
+++
@@ -1,4 +0,0 @@
-class Model():
-   #create Model based on XES-File provided by View
-   def __init__(self) -> None:
-      pass 
--- controller.py
+++ controller.py
@@ -1,4 +1,10 @@
+from  View.dashboard import Dashboard
+
 class Controller():
     def __init__(self) -> None:
-        pass
+        self.dashboard = Dashboard()
+
+if __name__ == '__main__':
+    c = Controller()
+    c.dashboard.app.run(debug=True)
     
