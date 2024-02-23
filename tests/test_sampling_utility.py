ae5523c9bb7f7b274a57f80c7774c6220be3516a
RobertJunghanns <51117160+RobertJunghanns@users.noreply.github.com>
2024-02-23 13:35:50 +0100

    refactor

--- tests/test_sampling_utility.py
+++ tests/test_sampling_utility.py
@@ -7,7 +7,7 @@ import pm4py
 import warnings
 import pandas as pd
 import pandas.testing as pdt
-from src.framework.utility import sampling_utility
+from src.framework.utility import activity_duration_estimation
 from src.framework.utility import xes_utility
 
 
@@ -28,31 +28,31 @@ class TestSamplingUtilityFunctions(unittest.TestCase):
         cls.case_ids = ['case1', 'case2', 'case3', 'case4', 'case5']
 
     def test_get_n_case_ids_less(self):
-        sampled_ids = sampling_utility.get_n_case_ids(self.case_ids, 3)
+        sampled_ids = activity_duration_estimation.get_n_case_ids(self.case_ids, 3)
         self.assertEqual(len(sampled_ids), 3)
 
     def test_get_n_case_ids_more(self):
-        sampled_ids = sampling_utility.get_n_case_ids(self.case_ids, len(self.case_ids))
+        sampled_ids = activity_duration_estimation.get_n_case_ids(self.case_ids, len(self.case_ids))
         self.assertEqual(len(sampled_ids), len(self.case_ids))
 
     def test_get_n_events_less(self):
-        sampled_events = sampling_utility.get_n_events(self.event_log, 5)
+        sampled_events = activity_duration_estimation.get_n_events(self.event_log, 5)
         self.assertEqual(len(sampled_events), 5)
 
     def test_get_n_events_more(self):
         filtered_event_log = self.event_log.dropna(subset=['org:resource'])
-        sampled_events = sampling_utility.get_n_events(filtered_event_log, len(filtered_event_log))
+        sampled_events = activity_duration_estimation.get_n_events(filtered_event_log, len(filtered_event_log))
         self.assertEqual(len(sampled_events), len(filtered_event_log))
 
     def test_get_trace(self):
-        sampled_trace = sampling_utility.get_trace(self.event_log, '173691')
+        sampled_trace = activity_duration_estimation.get_trace(self.event_log, '173691')
         expected_trace = self.event_log.loc[self.event_log['case:concept:name'] == '173691']
 
         pdt.assert_frame_equal(sampled_trace, expected_trace)
 
     def test_group_equal_timestamp_events_1(self):
-        trace = sampling_utility.get_trace(self.event_log, '173691')
-        grouped_trace = sampling_utility.group_equal_timestamp_events(trace)
+        trace = activity_duration_estimation.get_trace(self.event_log, '173691')
+        grouped_trace = activity_duration_estimation.group_equal_timestamp_events(trace)
 
         grouped_event_1 = grouped_trace[grouped_trace['concept:name'] == 'A_FINALIZED + O_SELECTED'] # same timestamps & resource
         grouped_event_2 = grouped_trace[grouped_trace['concept:name'] == 'O_SELECTED + O_CANCELLED'] # same timestamps & resource
@@ -65,8 +65,8 @@ class TestSamplingUtilityFunctions(unittest.TestCase):
         self.assertEqual(len(grouped_event_4), 0)
     
     def test_group_equal_timestamp_events_2(self):
-        trace = sampling_utility.get_trace(self.event_log_simple, '002')
-        grouped_trace = sampling_utility.group_equal_timestamp_events(trace)
+        trace = activity_duration_estimation.get_trace(self.event_log_simple, '002')
+        grouped_trace = activity_duration_estimation.group_equal_timestamp_events(trace)
 
         grouped_event_1_wrong = grouped_trace[grouped_trace['concept:name'] == 'B + C + D'] # same timestamps & resource but B has start event
         self.assertEqual(len(grouped_event_1_wrong), 0) # 'B + C + D' should not occure because B hast start event
@@ -84,8 +84,8 @@ class TestSamplingUtilityFunctions(unittest.TestCase):
         
         
     def test_add_activity_duration(self):
-        trace = sampling_utility.get_trace(self.event_log_simple, '001')
-        trace_activity_duration = sampling_utility.add_activity_durations_to_trace(trace)
+        trace = activity_duration_estimation.get_trace(self.event_log_simple, '001')
+        trace_activity_duration = activity_duration_estimation.add_activity_durations_to_trace(trace)
         durations = trace_activity_duration['duration']
 
         expected_durations = pd.Series(
@@ -96,8 +96,8 @@ class TestSamplingUtilityFunctions(unittest.TestCase):
         pdt.assert_series_equal(durations, expected_durations)
 
     def test_prepare_trace(self):
-        trace = sampling_utility.get_trace(self.event_log_simple, '001')
-        trace_prepared = sampling_utility.prepare_trace(trace)
+        trace = activity_duration_estimation.get_trace(self.event_log_simple, '001')
+        trace_prepared = activity_duration_estimation.prepare_trace(trace)
 
         self.assertEqual(len(xes_utility.get_column_names(trace)) + 1, len(xes_utility.get_column_names(trace_prepared)))
         self.assertLessEqual(len(trace_prepared), len(trace))
